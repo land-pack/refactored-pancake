a86cfb5dcca6775fa53354290e97e17af260b054
frank <frank@CN-MAC-013.local>
2019-06-20 11:29:40 +0800

    add new post

---
+++ new-post/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/shards-dashboards.1.1.0.css";
+
+import AddNewPost from "./views/AddNewPost";
+
+function App() {
+  return <AddNewPost />;
+}
+
+export default App;
